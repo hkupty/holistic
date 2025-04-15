20634be04bbc0d76f4d50bf32139bc6c34cb3ab3
Henry Kupty <hkupty@gmail.com>
2025-04-15 18:49:01 +0200

    feat: Introduce new parsers, move some data out

--- src/parsers.zig
+++ src/parsers.zig
@@ -1,28 +1,18 @@
 const std = @import("std");
 const mem = std.mem;
 const math = std.math;
-const testing = std.testing;
 const time = std.time;
+const hls = @import("hls.zig");
 
-const HLSVersion = struct { number: u8 };
+pub const ParseResult = union(enum) { empty: void, bin: []const u8, hls: hls.HLSField, seq: []const ParseResult };
 
-const HLSField = union(enum) {
-    Version: HLSVersion,
-};
-
-const ParseResult = union(enum) {
-    empty: void,
-    bin: []const u8,
-    hsl: HLSField,
-};
-
-const ParserState = struct {
+pub const ParserState = struct {
     buffer: []const u8,
     output: ParseResult,
 };
 
 /// Defines all possible errors that can happen when parsing
-const ParserError = error{
+pub const ParserError = error{
     /// This is returned when the parsed content doesn't match what the parser expects.
     /// It should be a recoverable error unless returned by a top-level parser.
     Mismatch,
@@ -37,7 +27,13 @@ const ParserError = error{
     ParserBuildError,
 };
 
-const ParseResultMapper = struct {
+pub const ParserBuildError = error{
+    ZeroLengthParserArray,
+    PreconditionError,
+    Unsupported,
+};
+
+pub const ParseResultMapper = struct {
     map: *const fn (ParseResult) ParserError!ParseResult,
 
     const identity: ParseResultMapper = .{ .map = struct {
@@ -45,9 +41,18 @@ const ParseResultMapper = struct {
             return state;
         }
     }.identity };
+
+    const last: ParseResultMapper = .{ .map = struct {
+        fn last(state: ParseResult) ParserError!ParseResult {
+            return switch (state) {
+                .seq => |s| s[s.len - 1],
+                inline else => state,
+            };
+        }
+    }.last };
 };
 
-fn debugParser(parser: Parser) void {
+pub fn debugParser(parser: Parser) void {
     switch (parser) {
         .str => |s| std.debug.print("String[{s}]\n", .{s.ref}),
         .seq => |s| {
@@ -69,6 +74,7 @@ fn debugParser(parser: Parser) void {
             }
             std.debug.print("]\n", .{});
         },
+        .cap => |s| std.debug.print("Capture[{s}]\n", .{s.terminator}),
     }
 }
 
@@ -79,11 +85,27 @@ const SequenceParser = struct {
 
     fn parse(self: SequenceParser, state: ParserState) ParserError!ParserState {
         var stateCursor = state;
-        for (self.inner) |parser| {
+        // HACK: This is to avoid heap allocation, should be revisited in the future
+        var intermediary = [_]ParseResult{ParseResult.empty} ** 32;
+        for (self.inner, 0..) |parser, ix| {
             stateCursor = try parser.parse(stateCursor);
+            intermediary[ix] = stateCursor.output;
         }
 
-        return .{ .buffer = stateCursor.buffer, .output = try self.handler.map(stateCursor.output) };
+        return .{
+            .buffer = stateCursor.buffer,
+            .output = try self.handler.map(.{ .seq = intermediary[0..self.inner.len] }),
+        };
+    }
+
+    fn map(comptime self: SequenceParser, handler: ParseResultMapper) SequenceParser {
+        return .{ .inner = self.inner, .handler = handler };
+    }
+
+    fn dropPrefix(comptime self: SequenceParser, prefix: []const u8) ParserError!SequenceParser {
+        const split = try self.inner[0].split(prefix.len);
+
+        return .{ .inner = split[1..] ++ self.inner[1..], .handler = self.handler };
     }
 
     fn flatConcat(comptime left: Parser, comptime right: Parser) SequenceParser {
@@ -121,6 +143,7 @@ const SequenceParser = struct {
 
             break :resolve inner;
         };
+        // TODO: Figure out which handler to use or combine
         return .{ .inner = &inner };
     }
 
@@ -147,6 +170,11 @@ const SequenceParser = struct {
 /// A parser that matches against a constant string
 const StringParser = struct {
     ref: []const u8,
+    handler: ParseResultMapper = .identity,
+
+    fn map(comptime self: StringParser, handler: ParseResultMapper) StringParser {
+        return .{ .ref = self.ref, .handler = handler };
+    }
 
     fn parse(self: *const StringParser, state: ParserState) ParserError!ParserState {
         if (self.ref.len > state.buffer.len) {
@@ -158,7 +186,7 @@ const StringParser = struct {
         if (mem.eql(u8, self.ref, target)) {
             return .{
                 .buffer = state.buffer[self.ref.len..state.buffer.len],
-                .output = .{ .bin = self.ref },
+                .output = try self.handler.map(.{ .bin = self.ref }),
             };
         } else {
             // std.debug.print("Got {s} but it doesn't match {s}\n", .{ target, self.ref });
@@ -175,6 +203,34 @@ const StringParser = struct {
 
     fn peek(self: *const StringParser) ?u8 {
         return self.ref[0];
+    }
+};
+
+const CaptureParser = struct {
+    terminator: []const u8,
+    optional: bool = false,
+
+    fn parse(self: CaptureParser, state: ParserState) ParserError!ParserState {
+        if (self.terminator.len == 0) {
+            return .{ .buffer = &.{}, .output = .{ .bin = state.buffer } };
+        }
+
+        if (state.buffer.len == 0) {
+            return ParserError.PrematureEndOfInput;
+        }
+
+        const ix = std.mem.indexOf(u8, state.buffer, self.terminator) orelse {
+            return switch (self.optional) {
+                true => .{ .buffer = &.{}, .output = .{ .bin = state.buffer } },
+                false => ParserError.Mismatch,
+            };
+        };
+
+        return .{ .buffer = state.buffer[ix + 1 ..], .output = .{ .bin = state.buffer[0..ix] } };
+    }
+
+    fn peek(_: CaptureParser) ?u8 {
+        return null;
     }
 };
 
@@ -250,10 +306,49 @@ const SelectParser = struct {
     }
 };
 
-const Parser = union(enum) {
+const RepeatParser = struct {
+    times: usize,
+    inner: *const Parser,
+    handler: ParseResultMapper = .identity,
+
+    fn parse(self: RepeatParser, state: ParserState) ParserError!ParserState {
+        var partial = [_]ParseResult{ParseResult.empty} ** 32;
+        var cursor = state;
+
+        const offset = off: {
+            for (0..self.times) |ix| {
+                cursor = self.inner.parse(cursor) catch |err| {
+                    switch (err) {
+                        ParserError.PrematureEndOfInput => break :off (self.times - ix),
+                        inline else => return err,
+                    }
+                };
+                partial[ix] = cursor.output;
+            }
+            break :off 0;
+        };
+
+        return .{
+            .buffer = cursor.buffer,
+            .output = try self.handler.map(.{ .seq = partial[0..(self.times - offset)] }),
+        };
+    }
+
+    fn map(comptime self: RepeatParser, handler: ParseResultMapper) RepeatParser {
+        return .{
+            .times = self.times,
+            .inner = self.inner,
+            .handler = handler,
+        };
+    }
+};
+
+pub const Parser = union(enum) {
     str: StringParser,
     seq: SequenceParser,
     sel: SelectParser,
+    cap: CaptureParser,
+    rep: RepeatParser,
 
     pub fn parseRaw(self: Parser, input: []const u8) ParserError!ParserState {
         const initialState = ParserState{ .buffer = input, .output = ParseResult.empty };
@@ -268,6 +363,7 @@ const Parser = union(enum) {
     }
 
     fn concat(self: Parser, other: Parser) Parser {
+        // TODO: refactor
         return switch (self) {
             inline else => |p| p.concat(other),
         };
@@ -287,16 +383,27 @@ const Parser = union(enum) {
         };
     }
 
+    pub fn map(comptime self: Parser, handler: ParseResultMapper) Parser {
+        return switch (self) {
+            .str => |p| .{ .str = p.map(handler) },
+            .seq => |p| .{ .seq = p.map(handler) },
+            .rep => |p| .{ .rep = p.map(handler) },
+            inline else => unreachable,
+        };
+    }
+
     pub fn split(comptime self: Parser, comptime ix: usize) ParserError![2]Parser {
         comptime switch (self) {
             .seq => unreachable, // TODO: Implement
             .sel => unreachable, // TODO: Implement
+            .cap => unreachable,
+            .rep => unreachable,
             .str => |strParser| if (ix > strParser.ref.len) {
                 return ParserError.ParserBuildError;
             } else {
                 return [_]Parser{
-                    .{ .str = .{ .ref = strParser.ref[0..ix] } },
-                    .{ .str = .{ .ref = strParser.ref[ix..] } },
+                    .{ .str = .{ .ref = strParser.ref[0..ix], .handler = strParser.handler } },
+                    .{ .str = .{ .ref = strParser.ref[ix..], .handler = strParser.handler } },
                 };
             },
         };
@@ -305,6 +412,18 @@ const Parser = union(enum) {
     /// Produces a new parser of type `StringParser` which will match against the supplied `ref`
     pub fn Str(comptime str: []const u8) Parser {
         return .{ .str = StringParser{ .ref = str } };
+    }
+
+    pub fn Capture(comptime terminator: []const u8) Parser {
+        return .{ .cap = .{ .terminator = terminator, .optional = false } };
+    }
+
+    pub fn MaybeCapture(comptime terminator: []const u8) Parser {
+        return .{ .cap = .{ .terminator = terminator, .optional = true } };
+    }
+
+    pub fn Seq(comptime parsers: []const Parser) Parser {
+        return .{ .seq = .{ .inner = parsers } };
     }
 
     /// Produces a new Parser which could be `SequenceParser`, but if all arguments are of the same
@@ -321,6 +440,10 @@ const Parser = union(enum) {
 
     pub fn Select(comptime parsers: []const Parser) ParserError!Parser {
         return .{ .sel = try SelectParser.buildFrom(parsers) };
+    }
+
+    pub fn Repeat(comptime parser: Parser, times: usize) Parser {
+        return .{ .rep = .{ .inner = &parser, .times = times } };
     }
 
     pub fn Zip(comptime parsers: []const Parser) ParserError!Parser {
@@ -369,15 +492,19 @@ const Parser = union(enum) {
             for (parsers) |p| {
                 if ((p.ref().len - commonPrefix.len) > 0) {
                     switch (p) {
-                        .str => |str| newParsers[ix] = Parser.Str(str.ref[commonPrefix.len..]),
-                        inline else => unreachable, // TODO: fix sequence
+                        .str => {
+                            const spl = p.split(commonPrefix.len) catch unreachable;
+                            newParsers[ix] = spl[1];
+                        },
+                        .seq => |seq| newParsers[ix] = .{ .seq = seq.dropPrefix(commonPrefix) catch unreachable },
+                        inline else => unreachable,
                     }
                     ix = ix + 1;
                 }
             }
 
             const select = try Parser.Select(&newParsers);
-            const seq = Parser.Sequence(&.{ Parser.Str(commonPrefix), select });
+            const seq: Parser = .{ .seq = .{ .inner = &.{ Parser.Str(commonPrefix), select }, .handler = .last } };
 
             break :init seq;
         };
@@ -385,6 +512,7 @@ const Parser = union(enum) {
 };
 
 test "Basic parser testing" {
+    const testing = std.testing;
     const basic = Parser.Str("hello");
 
     // Successfully parses into a ParseResult array list
@@ -401,7 +529,18 @@ test "Basic parser testing" {
     try testing.expectError(ParserError.Mismatch, basic.parseRaw("world"));
 }
 
+test "Capture parser" {
+    const testing = std.testing;
+    const capture = Parser.Capture(";");
+    const result = try capture.parseRaw("Hello;");
+    try switch (result.output) {
+        .bin => |data| testing.expectEqualStrings("Hello", data),
+        else => testing.expect(false),
+    };
+}
+
 test "Basic splitting" {
+    const testing = std.testing;
     const helloworld = comptime Parser.Str("helloworld");
     const split = try comptime helloworld.split(5);
     // const hello = comptime Parser.Str("hello", ParseResult.empty);
@@ -417,6 +556,7 @@ test "Basic splitting" {
 }
 
 test "sequence on parser strings concatenate them" {
+    const testing = std.testing;
     const hello = comptime Parser.Str("hello");
     const space = comptime Parser.Str(" ");
     const world = comptime Parser.Str("world");
@@ -435,6 +575,7 @@ test "sequence on parser strings concatenate them" {
 }
 
 test "select parser" {
+    const testing = std.testing;
     const p1 = comptime Parser.Str("alpha");
     const p2 = comptime Parser.Str("beta");
     const p3 = comptime Parser.Str("gamma");
@@ -447,24 +588,67 @@ test "select parser" {
     const select = try Parser.Select(&.{ p1, p2, p3, p4, p5, p6 });
 
     const res1 = try select.parseRaw("alpha");
-    try testing.expectEqualDeep(ParseResult{ .bin = "alpha" }, res1.output);
+    try testing.expectEqualStrings("alpha", res1.output.bin);
 
     const res2 = try select.parseRaw("beta");
-    try testing.expectEqualDeep(ParseResult{ .bin = "beta" }, res2.output);
+    try testing.expectEqualStrings("beta", res2.output.bin);
 
     try testing.expectError(ParserError.Mismatch, select.parseRaw("epsylon"));
 }
 
 test "zip parsers" {
+    const testing = std.testing;
+    const p1 = comptime Parser.Str("era");
+    const p2 = comptime Parser.Str("engine");
+
+    const glued = comptime try Parser.Zip(&.{ p1, p2 });
+    const zipped = comptime glued.map(.identity);
+
+    var result = try zipped.parseRaw("engine");
+    try testing.expectEqualDeep(ParseResult{ .seq = &.{ .{ .bin = "e" }, .{ .bin = "ngine" } } }, result.output);
+    result = try zipped.parseRaw("era");
+    try testing.expectEqualDeep(ParseResult{ .seq = &.{ .{ .bin = "e" }, .{ .bin = "ra" } } }, result.output);
+}
+
+test "putting it all together" {
+    const testing = std.testing;
     const p1 = comptime Parser.Str("era");
     const p2 = comptime Parser.Str("engine");
 
     const zipped = try Parser.Zip(&.{ p1, p2 });
+    const updated: Parser = build: switch (zipped) {
+        .seq => |s| {
+            const lambda = struct {
+                fn handler(state: ParseResult) ParserError!ParseResult {
+                    return .{ .hls = .{ .Version = .{ .number = @intCast(state.seq[1].bin.len) } } };
+                }
+            };
+            break :build .{ .seq = .{ .inner = s.inner, .handler = .{ .map = lambda.handler } } };
+        },
+        inline else => unreachable,
+    };
 
-    var result = try zipped.parseRaw("engine");
-    try testing.expectEqualDeep(ParseResult{ .bin = "ngine" }, result.output);
-    result = try zipped.parseRaw("era");
-    try testing.expectEqualDeep(ParseResult{ .bin = "ra" }, result.output);
+    var result = try updated.parseRaw("engine");
+    try testing.expectEqualDeep(ParseResult{ .hls = .{ .Version = .{ .number = 5 } } }, result.output);
+    result = try updated.parseRaw("era");
+    try testing.expectEqualDeep(ParseResult{ .hls = .{ .Version = .{ .number = 2 } } }, result.output);
+}
+
+test "repeating parser" {
+    const testing = std.testing;
+    const base = comptime Parser.Str("Yes!");
+    const repeat = comptime Parser.Repeat(base, 3);
+
+    const result = try repeat.parseRaw("Yes!Yes!Yes!");
+
+    try testing.expectEqual(0, result.buffer.len);
+    var ix: usize = 0;
+    for (result.output.seq) |partial| {
+        try testing.expectEqualStrings("Yes!", partial.bin);
+        ix += 1;
+    }
+
+    try testing.expectEqual(3, ix);
 }
 
 fn bench_parser(name: []const u8, parser: *const Parser, data: []const u8, comptime max_loops: usize) !void {
